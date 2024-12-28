38d031d93a50cd0c9ea4d642205be6a4138f6a24
mariambuzaladze <mariambuzaladze10@example.come>
2024-12-28 23:37:16 +0400

    fix: add features to sorted names

--- name_sorter/file.py
+++ name_sorter/file.py
@@ -1,7 +1,10 @@
-with open("names.txt","r") as file:
+with open("names.txt", "r") as file:
     names = [name.strip() for name in file.readlines()]
 
+sorted_names = sorted(names)
 
-with open("sorted_names.txt","w") as file:
-    for name in sorted(names):
-        file.write(name+"\n")
+with open("sorted_names.txt", "w") as file:
+    file.write(f"Total of {len(sorted_names)} names\n")
+    file.write("-" * 17 + "\n")
+    for name in sorted_names:
+        file.write(name + "\n")
